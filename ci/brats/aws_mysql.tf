42d8dc620a38f5e6101598991ae599f80a376018
Joshua Aresty <joshua.aresty@emc.com>
2018-10-18 11:53:32 -0700

    Add log_bin_trust_function_creators to db parameter group
    Signed-off-by: Denis Langer <denis.langer@sap.com>

--- ci/brats/aws_mysql.tf
+++ ci/brats/aws_mysql.tf
@@ -2,6 +2,26 @@ variable "rds_mysql_username" {}
 variable "rds_mysql_password" {}
 variable "rds_mysql_databasename" {}
 
+resource "aws_db_parameter_group" "default" {
+  name   = "brats-mysql"
+  family = "mysql5.7"
+
+  parameter {
+    name = "log_bin_trust_function_creators"
+    value = 1
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8"
+  }
+}
+
 resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
   storage_type           = "gp2"
@@ -12,7 +32,7 @@ resource "aws_db_instance" "mysql" {
   name                   = "${var.rds_mysql_databasename}"
   username               = "${var.rds_mysql_username}"
   password               = "${var.rds_mysql_password}"
-  parameter_group_name   = "default.mysql5.7"
+  parameter_group_name   = "brats-mysql"
   vpc_security_group_ids = ["${aws_security_group.allow-db-access.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
   publicly_accessible    = true
