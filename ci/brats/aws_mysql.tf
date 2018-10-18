8f275283f80ff7cb948527b598e886748e7870e6
Denis Langer <denis.langer@sap.com>
2018-10-18 16:07:19 -0700

    Try to disable binary logging by downgrading mysql to 5.5
    Signed-off-by: Joshua Aresty <joshua.aresty@emc.com>

--- ci/brats/aws_mysql.tf
+++ ci/brats/aws_mysql.tf
@@ -6,14 +6,13 @@ resource "aws_db_instance" "mysql" {
   allocated_storage      = 10
   storage_type           = "gp2"
   engine                 = "mysql"
-  engine_version         = "5.7"
+  engine_version         = "5.5"
   instance_class         = "db.t2.micro"
   skip_final_snapshot    = true
   name                   = "${var.rds_mysql_databasename}"
   username               = "${var.rds_mysql_username}"
   password               = "${var.rds_mysql_password}"
-  parameter_group_name   = "brats-mysql"
-  backup_retention_period = 0
+  parameter_group_name   = "default.mysql5.5"
   vpc_security_group_ids = ["${aws_security_group.allow-db-access.id}"]
   db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
   publicly_accessible    = true
